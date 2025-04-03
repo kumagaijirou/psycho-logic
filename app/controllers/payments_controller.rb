88b081a12fe22d902fe2fa291be68527a80dc554
kumagaijirou <55349713+kumagaijirou@users.noreply.github.com>
2025-04-03 15:18:25 +0000

    Fix checkout cancel_url bug

--- app/controllers/payments_controller.rb
+++ app/controllers/payments_controller.rb
@@ -5,16 +5,14 @@ class PaymentsController < ApplicationController
       line_items: [{
         price_data: {
           currency: 'jpy',
-          unit_amount: 500, # ← 金額（例：500円）
-          product_data: {
-            name: 'おまけ記事購入'
-          }
+          unit_amount: 500,
+          product_data: { name: 'おまけ記事購入' }
         },
         quantity: 1
       }],
       mode: 'payment',
-      success_url: "https://psycho-logic.jp/?success=true",
-      cancel_url:  "https://psycho-logic.jp/?canceled=true"
+      success_url: "https://psycho-logic.jp/success",
+      cancel_url:  "https://psycho-logic.jp/cancel"
     )
 
     redirect_to session.url, allow_other_host: true
