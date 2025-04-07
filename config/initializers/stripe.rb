86334724050ce73d2a412decc2b9f0b967e7974f
kumagaijirou <55349713+kumagaijirou@users.noreply.github.com>
2025-04-07 05:16:20 +0000

    Fix stripe

--- config/initializers/stripe.rb
+++ config/initializers/stripe.rb
@@ -1,1 +1,5 @@
-Stripe.api_key = ENV['STRIPE_SECRET_KEY']
+if ENV['STRIPE_SECRET_KEY'].present?
+  Stripe.api_key = ENV['STRIPE_SECRET_KEY']
+else
+  Rails.logger.warn "Stripe secret key is missing!"
+end
