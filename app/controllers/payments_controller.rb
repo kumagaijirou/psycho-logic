4316cbcb407aa7033e1f69423f46311e22905774
kumagaijirou <55349713+kumagaijirou@users.noreply.github.com>
2025-04-07 05:02:22 +0000

    Add stripe4

--- app/controllers/payments_controller.rb
+++ app/controllers/payments_controller.rb
@@ -1,4 +1,6 @@
 class PaymentsController < ApplicationController
+  before_action :logged_in_user, only: [:success]
+  before_action :correct_user,   only: [:success]
 
   def checkout
     Rails.logger.info "✅ checkoutアクションに入りました"
@@ -23,5 +25,40 @@ class PaymentsController < ApplicationController
     )
   
     redirect_to session.url, allow_other_host: true
+  end
+
+  def success
+    session_id = params[:session_id] # Stripe Checkoutから受け取る
+
+    # すでに処理済みならスキップ（重複防止）
+    return if PointLog.exists?(service_id: session_id)
+
+    @user = User.find(current_user.id)
+    @user.dice_point += 1000
+    @user.save
+    PointLog.create(
+      user_id: @user.id,
+      service_name: "その他",
+      category: "ポイントの購入",
+      dice_point: 1000,
+      service_id: session_id
+    )
+  end
+
+  private
+    # ログイン済みユーザーかどうか確認
+    def logged_in_user
+      unless logged_in?
+        store_location
+        flash[:danger] = "Please log in."
+        redirect_to login_url, status: :see_other
+      end
+    end
+
+    # 正しいユーザーかどうか確認
+    def correct_user
+      @user = User.find(current_user.id)
+      redirect_to(root_url, status: :see_other) unless current_user?(@user)
+    end
   end
 end
